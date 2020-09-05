a56f22c5f1c71755e12ba647df573e3cd8f3b844
vapvin <vins@vins.dev>
2020-09-06 01:00:02 +0900

    update, view

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate yew;
+
+use yew::html::*;
+
+struct Model {
+    input: String,
+    todos: Vec<String>,
+}
+
+enum Msg {
+    Add,
+    Update(String),
+    Remove(usize),
+    RemoveAll,
+    Nothing,
+}
+
+fn update(_: &mut Context<Msg>, model: &mut Model, msg: Msg){
+    match msg {
+        Msg::Add => {}
+        Msg::Update(s) => {}
+        Msg::Remove(i) => {}
+        Msg::RemoveAll => {}
+        Msg::Nothing => {}
+    }
+}
+
+fn view(model: &Model) -> Html<Msg> {
+    html! {
+        
+    }
+}
+
+fn main(){
+    println!("Hello, World");
 }
